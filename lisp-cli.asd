3584aaa5ecccb89dd913908f792d356a1ea1f6bd
chee <yay@chee.party>
2023-02-24 21:05:36 +0000

    add command system (also ql command)

--- lisp-cli.asd
+++ lisp-cli.asd
@@ -4,5 +4,5 @@
 	:build-operation "program-op"
 	:build-pathname "lisp"
 	:entry-point "lisp-cli:main"
-	:depends-on (#:str #:uiop)
+	:depends-on (#:str #:uiop #:quicklisp)
 	:components ((:file "lisp-cli")))
--- lisp-cli.lisp
+++ lisp-cli.lisp
@@ -3,8 +3,11 @@
   (:export main))
 (in-package :lisp-cli)
 
-(defun lisp-help (&rest args)
-  (declare (ignore args))
+(defvar *commands* nil)
+
+(defcommand help
+  "You're looking at it!"
+  (declare (ignore arguments))
   (princ "
 Lisp CLI.
 =========
@@ -12,42 +15,71 @@ by rabbits for rabbits
 ----------------------
 
 Commands:
+")
+  (loop for (name fn) on *commands* by #'cddr while fn
+    do (format t " lisp ~A ~A~%" (string-downcase name) (documentation fn 'function))))
 
-	lisp compile <SYSTEM> - Run `asdf:make' on SYSTEM
-	lisp eval '<LISP-FORMS>' - Evaluate some lisp (quietly) with sbcl
-"))
+(defmacro defcommand (command-name &rest body)
+  (let ((command-fn
+          (intern (concatenate 'string
+            "CLI-COMMAND-"
+            (symbol-name command-name)))))
+    `(prog1
+       (defun ,command-fn (&rest arguments)
+         ,@body)
+       (setf (getf *commands* ,(intern (symbol-name command-name) :keyword)) #',command-fn))))
 
-(defun lisp-compile (&rest args)
-  (let* ((system-string (first args))
-          (system (intern (string-upcase system-string) :keyword)))
+(defcommand build
+  	"<system> - Run `asdf:make' on SYSTEM"
+  (let* ((system-string (first arguments))
+          (system
+            (intern (string-upcase system-string) :keyword)))
+    (unless system-string
+      (cli-command-help)
+      (fresh-line uiop:*stderr*)
+      (princ "No system name provided to build command!" uiop:*stderr*)
+      (fresh-line uiop:*stderr*)
+      (uiop:quit 1))
     (ql:quickload system)
     (asdf:make system)))
 
-(defun lisp-eval (&rest args)
+(defcommand ql
+  	"<package> - Install package with quicklisp"
+  (let* ((package-string (first arguments))
+          (package
+            (intern (string-upcase package-string) :keyword)))
+    (unless package-string
+      (cli-command-help)
+      (fresh-line uiop:*stderr*)
+      (princ "No package name provided to ql command!" uiop:*stderr*)
+      (fresh-line uiop:*stderr*)
+      (uiop:quit 1))
+    (ql:quickload package)))
+
+
+(defcommand eval
+  "<expression> [expression...] - Evaluate some lisp (quietly) with sbcl."
   (uiop:run-program
     `("sbcl"
        "--noinform"
        "--no-userinit"
-       "--eval" ,(first args)
+       ,@(append (mapcar (lambda (form) (list "--eval" form)) arguments))
        "--quit")
     :output :interactive))
 
-(defun lisp-run (&rest args)
+(defcommand run
+  "<file> [file...] - Load and run lisp files (quietly) with sbcl"
   (uiop:run-program
     `("sbcl"
        "--noinform"
        "--no-userinit"
-       "--eval" ,(first args)
+       ,@(append (mapcar (lambda (form) (list "--load" form)) arguments))
        "--quit")
     :output :interactive))
 
 (defun main nil
-  (let* ((arguments (uiop:command-line-arguments))
-          (command (first arguments))
-          (sub-arguments (rest arguments)))
-    (apply
-      (cond
-        ((string= command "compile") #'lisp-compile)
-        ((string= command "eval") #'lisp-eval)
-        (t #'lisp-help))
-      sub-arguments)))
+  (let* ((argv (uiop:command-line-arguments))
+          (command-name (first argv))
+          (arguments (rest argv))
+          (command (getf *commands* (intern (string-upcase command-name) :keyword))))
+    (if command (apply command arguments) (cli-command-help))))
