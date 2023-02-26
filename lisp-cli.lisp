7724a8e6414b99b417e2aeb760ee81c4357de761
chee <yay@chee.party>
2023-02-26 02:42:18 +0000

    slightly improve doomed build command

--- lisp-cli.lisp
+++ lisp-cli.lisp
@@ -5,21 +5,11 @@
 
 (defvar *commands* nil)
 
-(defcommand help
-  "You're looking at it!"
-  (declare (ignore arguments))
-  (princ "
-Lisp CLI.
-=========
-by rabbits for rabbits
-----------------------
-
-Commands:
-")
-  (loop for (name fn) on *commands* by #'cddr while fn
-    do (format t " lisp ~A ~A~%" (string-downcase name) (documentation fn 'function))))
+(defmacro die-unless (condition &rest msg)
+  `(unless ,condition (print-help-and-die ,@msg)))
 
 (defmacro defcommand (command-name &rest body)
+  "Create a command and add it to *COMMANDS*."
   (let ((command-fn
           (intern (concatenate 'string
             "CLI-COMMAND-"
@@ -29,55 +19,94 @@ Commands:
          ,@body)
        (setf (getf *commands* ,(intern (symbol-name command-name) :keyword)) #',command-fn))))
 
+;; TODO revisit when you know everything
 (defcommand build
-  	"<system> - Run `asdf:make' on SYSTEM"
+  	"<system> - Run `asdf:make' on SYSTEM."
+  (let* ((system-string (first arguments))
+         (system-indicator
+           (intern (string-upcase system-string) :keyword)))
+    (die-unless system-string "No system name provided to build command!")
+
+    (ql:quickload system-indicator)
+    (unless (depends-on-p (asdf:find-system system-indicator) "QUICKLISP")
+      (princ "Unbinding quicklisp.")
+      (fresh-line)
+      ;; Remove quicklisp from the image
+      (loop for x being the external-symbol of "QUICKLISP"
+        when (boundp x) do (makunbound x)
+        when (fboundp x) do (fmakunbound x)))
+    ;; Make sure asdf doesn't try using quicklisp to look things up
+    (let ((asdf:*system-definition-search-functions*
+            '(asdf/package-inferred-system:sysdef-package-inferred-system-search
+               asdf/system-registry:sysdef-central-registry-search
+               asdf/system-registry:sysdef-source-registry-search)))
+      (asdf:make system-indicator))))
+
+
+(defcommand ql
+  	"<system> - Install system with quicklisp"
   (let* ((system-string (first arguments))
           (system
             (intern (string-upcase system-string) :keyword)))
-    (unless system-string
-      (cli-command-help)
-      (fresh-line uiop:*stderr*)
-      (princ "No system name provided to build command!" uiop:*stderr*)
-      (fresh-line uiop:*stderr*)
-      (uiop:quit 1))
-    (ql:quickload system)
-    (asdf:make system)))
+    (die-unless system-string "No system name provided.")
+    (ql:quickload system)))
 
-(defcommand ql
-  	"<package> - Install package with quicklisp"
-  (let* ((package-string (first arguments))
-          (package
-            (intern (string-upcase package-string) :keyword)))
-    (unless package-string
-      (cli-command-help)
-      (fresh-line uiop:*stderr*)
-      (princ "No package name provided to ql command!" uiop:*stderr*)
-      (fresh-line uiop:*stderr*)
-      (uiop:quit 1))
-    (ql:quickload package)))
 
+(defun prefix-args (args prefix)
+  (let (list)
+    (mapc
+     (lambda (arg)
+        (setq list (append list (list prefix arg))))
+      args)
+    list))
 
 (defcommand eval
   "<expression> [expression...] - Evaluate some lisp (quietly) with sbcl."
   (uiop:run-program
     `("sbcl"
        "--noinform"
-       "--no-userinit"
-       ,@(append (mapcar (lambda (form) (list "--eval" form)) arguments))
+       ,@(prefix-args arguments "--eval")
        "--quit")
     :output :interactive))
 
+(defun depends-on-p (system dependency)
+  (member dependency
+    (mapcar #'symbol-name (asdf:component-sideway-dependencies system))
+    :test #'string=))
+
+
 (defcommand run
-  "<file> [file...] - Load and run lisp files (quietly) with sbcl"
+  "<file> [file...] - Load and run lisp files (quietly) with sbcl."
   (uiop:run-program
     `("sbcl"
        "--noinform"
-       "--no-userinit"
-       ,@(append (mapcar (lambda (form) (list "--load" form)) arguments))
+       ,@(prefix-args arguments "--load")
        "--quit")
     :output :interactive))
 
-(defun main nil
+
+(defun print-help-and-die (&rest errors)
+  (princ "Lisp CLI.")              (fresh-line)
+  (princ "=========")              (fresh-line)
+  (princ "by rabbits for rabbits") (fresh-line)
+  (princ "----------------------") (fresh-line) (fresh-line)
+  (princ "Commands:")              (fresh-line)
+
+  (loop for (name fn) on *commands* by #'cddr while fn
+    do (format t " lisp ~A ~A~%" (string-downcase name) (documentation fn 'function)))
+  (when errors
+    (format uiop:*stderr* "~%")
+    (dolist (msg errors) (fresh-line) (princ msg))
+    (uiop:quit 7)))
+
+(defcommand help
+  "- You're looking at it!"
+  (declare (ignore arguments))
+  (print-help-and-die))
+
+;; &rest _ for compatability with buildapp
+(defun main (&rest _)
+  (declare (ignore _))
   (let* ((argv (uiop:command-line-arguments))
           (command-name (first argv))
           (arguments (rest argv))
